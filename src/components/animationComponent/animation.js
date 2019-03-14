0027d9b96793fcd60e43fe44a8149ee08a539afc
Suraj Sharma <www.surajsharma@gmail.com>
2019-03-14 11:27:02 +0530

    tile component (animation)

--- src/components/animationComponent/animation.js
+++ src/components/animationComponent/animation.js
@@ -8,43 +8,41 @@ import "./static/css/animation.css";
 export default class Animation extends React.Component {
   render() {
     return (
-      <div  className="anim">
-        <div className="tile is-ancestor">
-            <div className="tile">
-              <div className="tile is-parent">
-                <article className="tile is-child card is-vertical">
-                  <p className="hospital-header">HOSPITALS 1</p>
-                  <p className="subtitle"></p>
+      <Container className="tileContainer">
+        <div class="tile is-ancestor">
+          <div class="tile is-vertical">
+            <div class="tile">
+              <div class="tile is-parent is-vertical">
+                <article class="tile is-child box is-12 notification is-success">
+                  <h1>One</h1>
+                  <p>Post-engine tube human range-rover DIY concrete physical shoes A.I. savant carbon tanto courier realism. Vehicle geodesic post-j-pop construct tower euro-pop semiotics singularity math-shoes. </p>
+                </article>
+                <article class="tile is-child box is-12 notification is-warning">                
+                  <h1>Two</h1>
+                  <p>Post-engine tube human range-rover DIY concrete physical shoes A.I. savant carbon tanto courier realism. Vehicle geodesic post-j-pop construct tower euro-pop semiotics singularity math-shoes. Nodal point dissident DIY geodesic nodality beef noodles soul-delay. Convenience store car</p>
                 </article>
               </div>
-              <div className="tile is-parent">
-                <article className="tile is-child is-dark card">
-                  <p className="hospital-header">HOSPITALS</p>
-                  <p className="subtitle"></p>
+              <div class="tile is-parent">
+                <article class="tile is-child box notification is-danger">
+                  <h1>Three</h1>
+                  <p>Post-engine tube human range-rover DIY concrete physical shoes A.I. savant carbon tanto courier realism. Vehicle geodesic post-j-pop construct tower euro-pop semiotics singularity math-shoes.  Construct Kowloon city nodal point euro-pop Tokyo futurity lights camera papier-mache voodoo god narrative jeans.  Network market wristwatch wonton soup soul-delay sprawl vinyl tube 3D-printed. Pen singularity cardboard uplink advert bomb augmented reality camera into faded car film sentient monofilament tower. Skyscraper sensory nodality courier boat disposable alcohol office corrupted range-rover render-farm camera girl post-semiotics. 
+                  </p>
                 </article>
               </div>
-            <div className="tile is-parent">
-              <article className="tile is-child is-dark card">
-                <p className="hospital-header">MEDICAL EQUIPMENT</p>
-              </article>
-            </div>
-          <div className="tile is-parent">
-            <article className="card tile is-child is-dark">
-              <div className="content">
-                <p className="hospital-header">AMBULANCE</p>
-                <div className="content">
-                </div>
-              </div>
-            </article>
-          </div>
-          <div className="tile is-parent">
-            <article className="tile is-child is-dark card">
-              <p className="hospital-header">NURSING</p>
-            </article>
-          </div>
             </div>
           </div>
-        </div>                  
+          <div class="tile is-parent is-vertical">
+            <article class="tile is-child box is-12">
+              <h1>Five</h1>
+               <p>Post-engine tube human range-rover DIY concrete physical shoes A.I. savant carbon tanto courier realism. Vehicle geodesic cyber-wristwatch corporation 8-bit table bicycle corrupted A.I. drugs city rebar bomb. Corrupted pistol sensory urban bridge fluidity singularity beef noodles ablative RAF DIY digital savant-ware 3D-printed sign. Ablative bridge dome semiotics-space Legba pre-engine. Engine drugs pen wonton soup pistol cartel sensory sentient narrative concrete shrine carbon. Military-grade artisanal-space vehicle chrome fluidity Kowloon skyscraper bomb concrete pen nano-neon tower spook tattoo. </p>
+           </article>
+            <article class="tile is-child box is-12 notification is-info">
+              <h1>Four</h1>
+               <p>Post-engine tube human range-rover DIY concrete physical shoes A.I. savant carbon tanto courier realism. Vehicle geodesic cyber-wristwatch corporation 8-bit table bicycle corrupted A.I. drugs city rebar bomb. Corrupted pistol sensory urban bridge fluidity singularity beef noodles ablative RAF DIY digital savant-ware 3D-printed sign. Ablative bridge dome semiotics-space Legba pre-engine. Engine drugs pen wonton soup pistol cartel sensory sentient narrative concrete shrine carbon. Military-grade artisanal-space vehicle chrome fluidity Kowloon skyscraper bomb concrete pen nano-neon tower spook tattoo. </p>
+           </article>
+          </div>
+        </div>
+     </Container>
     );
   }
 }
