3aae2482df01974b0f7846e5f2e707b8e398c705
Suraj Sharma <www.surajsharma@gmail.com>
2019-03-15 15:14:21 +0530

    ui fixes

--- src/components/indexComponent/hospitalComponent/hospital.js
+++ src/components/indexComponent/hospitalComponent/hospital.js
@@ -6,39 +6,38 @@ import "./static/css/hospital.css";
 export default class Hospital extends React.Component {
   render() {
     return (
-            <Container style={{ width: "89em" }}>
+        <Container style={{ width: "89em" }}>
         <h4 className="header-name">HOSPITAL</h4>
         <div className="underscore" /> 
+
         <div className="tile is-ancestor nursingcontainer">
+
           <div className="tile is-vertical is-8">
             <div className="tile">
-              <div className="tile is-parent is-vertical hospital-card">
-                <article className="hos1 tile is-child notification is-primary card">
-                  <p className="hospital-header">HOSPITAL-1</p>
+              <div className="hos1 tile is-parent is-vertical">
+                <div className="hoschild tile is-child">
+                    <span className="hospital-header">HOSPITAL-1</span>
                   <p className="subtitle"></p>
-                </article>
+                </div>
               </div>
-              <div className="tile is-parent hospital-card">
-                <article className="hos3 tile is-child notification is-info card">
-                  <p className="hospital-header">HOSPITAL-3</p>
+              <div className="hos2 tile is-parent">
+                <article className="hoschild tile is-child">
+                  <p className="hospital-header">HOSPITAL-2</p>
                 </article>
               </div>
             </div>
-            <div className="tile is-parent hospital-card">
-              <article className="hos4 tile is-child notification is-danger card">
-                <p className="hospital-header">HOSPITAL-4</p>
+            <div className="hos3 tile is-parent">
+              <article className="hoschild tile is-child">
+                <p className="hospital-header">HOSPITAL-3</p>
               </article>
             </div>
           </div>
-          <div className="tile is-parent hospital-card">
-            <article className="hos5 card tile is-child notification is-success">
-              <div className="content">
-                <p className="hospital-header">HOSPITAL-5</p>
+          <div className="hos4 tile is-parent">
+              <div className="hoschild content">
+                <p className="hospital-header">HOSPITAL-4</p>
                 <div className="content">
-                  <mark>Soul-delay computer corporation render-farm bomb market dome plastic 8-bit boat face forwards nodal point hacker corrupted.-ware systema geodesic paranoid corporation 8-bit cyber-denim sensory euro-pop A.I.. Nodal point savant bicycle sentient urban table systemic into courier. Digital apophenia RAF marketing advert sensory sub-orbital assassin hacker wonton soup footage DIY.</mark>               
                 </div>
               </div>
-            </article>
           </div>
         </div>                  
       </Container>   
