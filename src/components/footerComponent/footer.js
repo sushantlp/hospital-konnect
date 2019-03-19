9068b97bcf50d0db8f533dea852a31ae0d50dd37
Suraj Sharma <www.surajsharma@gmail.com>
2019-03-19 11:44:07 +0530

    footer fixes

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -11,95 +11,88 @@ import {
 
 import "semantic-ui-css/semantic.min.css";
 import "./static/css/footer.css";
-
-import AppStore from "./static/img/apple-store.png";
-import GooglePlay from "./static/img/google-play.png";
+import AppStore from "./static/img/appstore_badge_en.svg";
+import GooglePlay from "./static/img/googleplay_badge_en.svg";
 
 export default class Footer extends React.Component {
   render() {
     return (
       <div className="footer-container">
         <Divider/>
-          <Grid doubling floated="left" centered stackable>
-            <Grid.Row columns="equal">
-              <Grid.Column floated="left">
-                <List horizontal link relaxed>
-                  <List.Item className='link'>
-                    <Link to="/terms">Terms of Service </Link>
+          <div className='columns'>
+          <div className='column is-8'>
+                <List className='list' horizontal link relaxed>
+                  <List.Item >
+                    <Link to="/terms">
+                    <p className='flink'>Terms of Service</p>
+                    </Link>
                   </List.Item>
                   <List.Item>
-                    <Link to="/privacy">Privacy Policy</Link>
+                    <Link to="/privacy">
+                      <p className='flink'>Privacy Policy</p>
+                    </Link>
                   </List.Item>
-
                   <List.Item>
-                    <Link to="/faq">FAQ's</Link>
+                    <Link to="/faq">
+                      <p className='flink'>FAQ's</p>
+                    </Link>
                   </List.Item>
                 </List>
-              </Grid.Column>
-              <Grid.Column floated="right" textAlign="right">
+              </div>
+              <div className='column is-4'>
                 <List horizontal link>
                   <List.Item
                     as="a"
-                    href="https://www.facebook.com/ballyhootoday/"
+                    href="https://www.facebook.com/"
                     target="_blank"
                   >
-                    <Icon circular name="facebook" />
+                    <i class="red facebook large icon"></i>
                   </List.Item>
-
                   <List.Item
                     as="a"
-                    href="https://www.instagram.com/ballyhootoday/"
+                    href="https://www.instagram.com/"
                     target="_blank"
                   >
-                    <Icon circular name="instagram" />
+                    <i class="red instagram large icon"></i>
                   </List.Item>
-
                   <List.Item
                     as="a"
-                    href="https://twitter.com/BallyhooToday"
+                    href="https://twitter.com/"
                     target="_blank"
                   >
-                    <Icon circular name="twitter" />
+                    <i class="red twitter large icon"></i>
                   </List.Item>
                 </List>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+              </div>
+            </div>
           <Divider />
-
-          <Grid doubling stackable columns={2}>
-            <Grid.Row>
-              <Grid.Column width={6} />
-              <Grid.Column>
-                <List horizontal link relaxed>
-                  <List.Item
-                    as="a"
-                    target="_blank"
-                    href="https://itunes.apple.com/in/app/ballyhoo-food-drinks-offers/id1138306421?mt=8"
-                  >
-                    <Image
-                      centered
-                      src={AppStore}
-                      style={{ width: "120px", height: "auto" }}
-                    />
-                  </List.Item>
-
-                  <List.Item
-                    as="a"
-                    target="_blank"
-                    href="https://play.google.com/store/apps/details?id=com.sense.today.ballyhoo"
-                  >
-                    <Image
-                      centered
-                      src={GooglePlay}
-                      style={{ width: "120px", height: "auto" }}
-                    />
-                  </List.Item>
-                </List>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-      </div>
+            <div>
+              <List horizontal link relaxed>
+                <List.Item
+                  as="a"
+                  target="_blank"
+                  href=""
+                >
+                  <Image
+                    centered
+                    src={AppStore}
+                    style={{ width: "120px", height: "auto" }}
+                  />
+                </List.Item>
+                <List.Item
+                  as="a"
+                  target="_blank"
+                  href=""
+                >
+                  <Image
+                    centered
+                    src={GooglePlay}
+                    style={{ width: "140px", height: "auto" }}
+                  />
+                </List.Item>
+              </List>
+            </div>
+          </div>
     );
   }
 }
